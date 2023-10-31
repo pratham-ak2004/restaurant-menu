5f84fca3e6edfafcf1e1d44bc9f382e910a6a016
pratham-ak2004 <pratham20442@gmail.com>
2023-10-31 18:07:18 +0530

    documentation : removed comments

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/navbar/navbar'
 import Cards from './components/layout/Cards'
-import Carousal from './components/layout/components/Carousal/Carousal';
 import Footer from './components/layout/components/Footer/Footer';
 
 
