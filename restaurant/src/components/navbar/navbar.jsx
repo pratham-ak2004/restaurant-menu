858959a00a4a2885b60045e681a84ddb9002c784
pratham-ak2004 <pratham20442@gmail.com>
2023-11-01 19:09:23 +0530

    completed navbar responsiveness

--- restaurant/src/components/navbar/navbar.jsx
+++ restaurant/src/components/navbar/navbar.jsx
@@ -40,19 +40,19 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="navbar-header">
-                <a className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <Link className="nav-link" to="/cards">
                   Menu
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
               </li>
             </ul>
           </div>
