281c7b60c2e3c4add0bcfbcb8b4db54bbfd00f67
your_username <your_email@example.com>
2023-11-14 22:54:37 +0530

    About Us added

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -5,7 +5,7 @@ import Cards from './components/layout/Cards';
 import Footer from './components/layout/components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Overview from "./components/layout/components/Overview/Overview";
-
+// import AboutUs from './components/layout/components/AboutUs/AboutUs';
 function App() {
 
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route exact path="/" element={<Overview image={"https://th.bing.com/th/id/OIP.fhWnuV2SzN_XbiSofYMK-AHaF-?pid=ImgDet&rs=1"} />}></Route>
           <Route exact path="/cards" element={<Cards />}></Route>
         </Routes>
+      {/* <AboutUs/> */}
       <Footer/>
     </div>
     </BrowserRouter>
---
+++ restaurant/src/components/layout/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./AboutUs.css";
+const AboutUs = () => {
+  return (
+    <div>
+        <div class="rectangle-div"></div>
+    <img class="rectangle-icon" alt="" src="./images/images (1).jpeg" />
+
+    <img class="desktop-1-child1" alt="" src="./images/images (2).jpeg" />
+
+    <img class="desktop-1-child2" alt="" src="./images/images (3).jpeg" />
+
+    <img class="desktop-1-child3" alt="" src="./images/images (4).jpeg" />
+         <div class="about-us">About us</div>
+    <div class="welcome-to-nmamit-container">
+      <p class="welcome-to-nmamit">
+        Welcome to NMAMIT DURGA Canteen, your one-stop shop for delicious and
+        affordable meals and snacks on campus. We are committed to providing
+        our students,faculty,and staff with a wide variety of high quality
+        food options that are both nutritios and satisfying .
+      </p>
+      <p class="welcome-to-nmamit">&nbsp;</p>
+      <p class="welcome-to-nmamit">
+        Our team of passionate chefs takes pride in preparing fresh,
+        high-quality meals using locally sourced ingredients whenever
+        possible. We believe that healthy eating is an essential part of a
+        successful college experience, and we strive to offer a wide range of
+        options to suit every palate and preference
+      </p>
+      <p class="welcome-to-nmamit">&nbsp;</p>
+      <p class="welcome-to-nmamit">
+        We understand that college students have busy schedules and tight
+        budgets, which is why we offer affordable pricing on all our menu
+        items. We believe that everyone should have access to healthy,
+        nutritious meals, and we're committed to making our canteen a place
+        where everyone feels welcome and valued.
+      </p>
+      <p class="welcome-to-nmamit">&nbsp;</p>
+      <p class="welcome-to-nmamit">
+        we're more than just a place to grab a bite to eat. We're a hub for
+        the college community, a place where students can come together to
+        socialize, recharge, and refuel for the challenges that lie ahead.
+        We're proud to be a part of your college experience, and we're
+        committed to providing you with the best possible dining experience
+        every time you visit
+      </p>
+    </div>
+    </div>
+  )
+}
+
+export default AboutUs
